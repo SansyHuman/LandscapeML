Fixed clustering to draw standard deviation of clustering accuracy.

clustering/main_sci_exp_cluster_pairwise.py
@@ -26,31 +26,27 @@ from common.sci_parser import SuperConformalIndex
 
 
 def build_data(sampler: TheorySampler, n_per_theory: int, n_iter: int, grid: np.ndarray, kde_bandwidth: float):
-    exp_data_per_theories = dict()
-    ac_data_per_theories = dict()
+    data_per_theories = dict()
 
     stats = sampler.get_theory_stats()
     theories = stats.select("Name").rdd.flatMap(lambda x: x).collect()
     for theory in theories:
-        exp_data_per_theories[theory] = [[] for _ in range(n_iter)]
-        ac_data_per_theories[theory] = [[] for _ in range(n_iter)]
+        data_per_theories[theory] = [[] for _ in range(n_iter)]
 
     for i in range(n_iter):
         sampled_data = sampler.get_manual_sample(theories, n_per_theory)
         rows = sampled_data.df.collect()
 
         for row in rows:
-            exp_data_per_theories[row["Name"]][i].append(SuperConformalIndex(row["SCI"]).featurize_dimensions(grid, kde_bandwidth))
-            ac_data_per_theories[row["Name"]][i].append([float(row["CentralChargeA"]), float(row["CentralChargeC"])])
+            data_per_theories[row["Name"]][i].append(SuperConformalIndex(row["SCI"]).featurize_dimensions(grid, kde_bandwidth))
 
         print(f"Data generation iteration {i + 1} completed.")
 
     for theory in theories:
         for i in range(n_iter):
-            exp_data_per_theories[theory][i] = np.stack(exp_data_per_theories[theory][i])
-            ac_data_per_theories[theory][i] = np.stack(ac_data_per_theories[theory][i])
+            data_per_theories[theory][i] = np.stack(data_per_theories[theory][i])
 
-    return exp_data_per_theories, ac_data_per_theories
+    return data_per_theories
 
 
 def cluster_pair(theory1_input: np.ndarray, theory2_input: np.ndarray):
@@ -133,32 +129,29 @@ if __name__ == '__main__':
     for i in range(n_theory):
         theories_dict[theories[i]] = i
 
-    accuracy_exp = [[0.0 for _ in range(n_theory)] for _ in range(n_theory)]
-    accuracy_ac = [[0.0 for _ in range(n_theory)] for _ in range(n_theory)]
+    accuracy = [[0.0 for _ in range(n_theory)] for _ in range(n_theory)]
+    stdev = [[0.0 for _ in range(n_theory)] for _ in range(n_theory)]
 
     pairs = [(theories[i], theories[j]) for i in range(n_theory) for j in range(i + 1, n_theory)]
     print(f"Number of pairs: {len(pairs)}")
 
-    exp_data_per_theory, ac_data_per_theory = build_data(cutoff_sampler, n_sample, n_iter, GRID, KDE_BANDWIDTH)
+    data_per_theory = build_data(cutoff_sampler, n_sample, n_iter, GRID, KDE_BANDWIDTH)
 
     def pair_calculation(theory1: str, theory2: str):
         print(f"Calculating accuracy for {theory1} and {theory2}")
-        acc_exp = []
-        acc_ac = []
+        acc = []
 
         for i in range(n_iter):
-            acc_exp.append(cluster_pair(exp_data_per_theory[theory1][i], exp_data_per_theory[theory2][i]))
-            acc_ac.append(cluster_pair_no_tsne(ac_data_per_theory[theory1][i], ac_data_per_theory[theory2][i]))
+            acc.append(cluster_pair(data_per_theory[theory1][i], data_per_theory[theory2][i]))
 
-        mean_acc_exp = float(np.mean(acc_exp))
-        accuracy_exp[theories_dict[theory1]][theories_dict[theory2]] = mean_acc_exp
-        accuracy_exp[theories_dict[theory2]][theories_dict[theory1]] = mean_acc_exp
-        print(f"Accuracy for {theory1} and {theory2} with exponent clustering: {mean_acc_exp}")
-
-        mean_acc_ac = float(np.mean(acc_ac))
-        accuracy_ac[theories_dict[theory1]][theories_dict[theory2]] = mean_acc_ac
-        accuracy_ac[theories_dict[theory2]][theories_dict[theory1]] = mean_acc_ac
-        print(f"Accuracy for {theory1} and {theory2} with a/c clustering: {mean_acc_ac}")
+        mean_acc= float(np.mean(acc))
+        stdev_acc = float(np.std(acc))
+        accuracy[theories_dict[theory1]][theories_dict[theory2]] = mean_acc
+        accuracy[theories_dict[theory2]][theories_dict[theory1]] = mean_acc
+        stdev[theories_dict[theory1]][theories_dict[theory2]] = stdev_acc
+        stdev[theories_dict[theory2]][theories_dict[theory1]] = stdev_acc
+        print(f"Stats for {theory1} and {theory2} with exponent clustering")
+        print(f"    mean: {mean_acc}, stdev: {stdev_acc}")
 
     with ThreadPoolExecutor(max_workers=mp.cpu_count()) as executor:
         executor.map(lambda arg: pair_calculation(*arg), pairs)
@@ -166,40 +159,34 @@ if __name__ == '__main__':
     save_dir = f"../data/clustering/{datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S")}"
     os.makedirs(save_dir, exist_ok=True)
 
-    with open(f"{save_dir}/sci_exp_cluster_pairwise_exponents.csv", 'w', newline='') as csv_file:
+    with open(f"{save_dir}/sci_exp_cluster_pairwise_acc.csv", 'w', newline='') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(["Theories"] + theories)
         for i in range(n_theory):
-            writer.writerow([theories[i]] + accuracy_exp[i])
+            writer.writerow([theories[i]] + accuracy[i])
 
-    with open(f"{save_dir}/sci_exp_cluster_pairwise_ac.csv", 'w', newline='') as csv_file:
+    with open(f"{save_dir}/sci_exp_cluster_pairwise_stdev.csv", 'w', newline='') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(["Theories"] + theories)
         for i in range(n_theory):
-            writer.writerow([theories[i]] + accuracy_ac[i])
-
-    accuracy_diff = np.abs(np.asarray(accuracy_exp) - np.asarray(accuracy_ac))
+            writer.writerow([theories[i]] + stdev[i])
 
     plt.style.use('default')
-    plt.rcParams['figure.figsize'] = (24, 12)
+    plt.rcParams['figure.figsize'] = (16, 12)
     plt.rcParams['font.size'] = 12
 
-    fig, ax = plt.subplots(nrows=1, ncols=3, squeeze=True)
-    fig.suptitle("Pairwise clustering accuracies")
+    fig, ax = plt.subplots(nrows=1, ncols=2, squeeze=True)
+    fig.suptitle("Pairwise clustering accuracies and stdevs")
 
     vmin, vmax = 0.5, 1.0
 
-    ax[0].set_title("Exponent clustering")
-    cmap_plot = ax[0].matshow(accuracy_exp, vmin=vmin, vmax=vmax, cmap='gray')
+    ax[0].set_title("Accuracies")
+    cmap_plot = ax[0].matshow(accuracy, vmin=vmin, vmax=vmax, cmap='gray')
     fig.colorbar(cmap_plot, ax=ax[0], shrink=0.7)
 
-    ax[1].set_title("a/c clustering")
-    cmap_plot = ax[1].matshow(accuracy_ac, vmin=vmin, vmax=vmax, cmap='gray')
+    ax[1].set_title(f"Stdevs iteration: {n_iter}")
+    cmap_plot = ax[1].matshow(stdev, vmin=0.0, vmax=np.max(stdev), cmap='gray_r')
     fig.colorbar(cmap_plot, ax=ax[1], shrink=0.7)
-
-    ax[2].set_title("Accuracy difference")
-    cmap_plot = ax[2].matshow(accuracy_diff, cmap='gray')
-    fig.colorbar(cmap_plot, ax=ax[2], shrink=0.7)
 
     plt.savefig(f'{save_dir}/sci_exp_cluster_pairwise.png')
     plt.show()